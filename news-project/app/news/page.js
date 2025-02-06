c89880596c1624384edc98d2a39bac70e5faf989
Francesco <franco.labate19@gmail.com>
2025-02-06 18:00:39 +0100

    minor fixes

---
+++ news-project/app/news/page.js
@@ -0,0 +1,14 @@
+const NewsPage= ()=> {
+    return (
+        <>
+          <h1>News Page</h1>
+            <ul>
+                <li>First Item </li>
+                <li>Second Item </li>
+                <li>Third Item </li>
+            </ul>
+        </>
+      
+    )
+}
+export default NewsPage; 
