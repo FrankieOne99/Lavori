8222626568e5747705ed2d0467a0f9d39a76d29e
Francesco <franco.labate19@gmail.com>
2025-02-07 14:37:41 +0100

    Adding new feature

--- news-project/app/news/[slug]/page.js
+++ news-project/app/news/[slug]/page.js
@@ -1,25 +1,26 @@
-
 import { DUMMY_NEWS } from "@/dummy-news";
+import Link from "next/link";
 import { notFound } from "next/navigation";
-const NewsDetail = ({params}) => {
-const newsSlug= params.slug
- const newsItem = DUMMY_NEWS.find(newsItem=>newsItem.slug===newsSlug)
+const NewsDetail = ({ params }) => {
+  const newsSlug = params.slug;
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
 
- if(!newsItem) {
-    notFound(); 
- }
-    return (
-        <article className="news-article">
-            <header>
-                <img src={`/images/news/${newsItem.image}`} alt={newsItem.title}/>
-            <h1>{newsItem.title}</h1>
-            <time dateTime={newsItem.date}>{newsItem.date}</time>
-            </header>
-        
-        <p>{newsItem.content}</p>
+  if (!newsItem) {
+    notFound();
+  }
+  return (
+    <article className="news-article">
+      <header>
+        <Link href={`/news/${newsItem.slug}/image`}>
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </Link>
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
 
-        </article>
-    )
-}
+      <p>{newsItem.content}</p>
+    </article>
+  );
+};
 
-export default NewsDetail; 
+export default NewsDetail;
