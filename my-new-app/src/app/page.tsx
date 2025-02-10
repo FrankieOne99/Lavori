250f63bca3992f4d05134e97379273e464f8c819
Francesco <franco.labate19@gmail.com>
2025-02-10 15:44:02 +0100

    create new project my-new-app

---
+++ my-new-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>This is my Homepage</h1>
+    </div>
+  );
+}
