1ed99a720f985fbca526dabd23e8d2e892987b8e
Francesco <franco.labate19@gmail.com>
2025-02-12 18:05:23 +0100

    Add new style for createSnippetPage

---
+++ snippets-project/src/app/snippets/new/page.tsx
@@ -0,0 +1,27 @@
+const SnippetCreatePage = () => {
+  return (
+    <form>
+      <h3 className="font-bold m-3">Create a new Snippet!</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input name="title" className="border p-2 w-full" id="title" />
+        </div>
+
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            code
+          </label>
+          <textarea name="code" className="border p-2 w-full" id="code" />
+        </div>
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default SnippetCreatePage;
