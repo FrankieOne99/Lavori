4501b5d2d23041028b46352fe46184b62706dddd
Francesco <franco.labate19@gmail.com>
2025-02-13 10:56:04 +0100

    Add new feature in snippet-project

--- snippets-project/src/app/page.tsx
+++ snippets-project/src/app/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import { db } from "@/db";
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippet) => {
+    return <li key={snippet.id}>{snippet.title}</li>;
+  });
+
   return (
     <>
-      <h1>This is the Homepage</h1>
+      <h1>List of Snippet: </h1>
+      <div>{renderedSnippets}</div>
     </>
   );
 }
--- snippets-project/src/app/snippets/new/page.tsx
+++ snippets-project/src/app/snippets/new/page.tsx
@@ -1,6 +1,29 @@
+import { db } from "@/db";
+import { redirect } from "next/navigation";
 const SnippetCreatePage = () => {
+  async function createSnippet(formData: FormData) {
+    //This need to be a server action!!
+    "use server";
+
+    //Check the user's inputs and make sure they're valid
+    const title = formData.get("title") as string; //I write them as "strings" because typescript can considered them as file
+    const code = formData.get("code") as string;
+
+    //Create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title: title,
+        code: code,
+      },
+    });
+    console.log(snippet);
+    //Redirect the ser back to the root route
+    redirect("/");
+  }
+
+  //Create snipppet is invoke every type a user submit the form
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold m-3">Create a new Snippet!</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
